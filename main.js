6488052cc80bffd5098e481f8ac1a623549b95ff
jwlee123 <jwlee@ymtech.co.kr>
2024-12-23 09:46:04 +0900

    ...

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain, ipcRenderer } = require('electron');
+const { autoUpdater } = require("electron-updater");
 const path = require('path');
 const url = require('url'); // URL 모듈 추가
 
+let win;
+
 function createWindow() {
-  const mainWindow = new BrowserWindow({
+  win = new BrowserWindow({
     width: 800,
     height: 600,
+    webPreferences: {
+      enableRemoteModule: true,
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
   });
 
-  mainWindow.loadURL(
+  win.loadURL(
     url.format({
       pathname: path.join(__dirname, 'react-app/build/index.html'),
       protocol: 'file:',
@@ -17,7 +25,34 @@ function createWindow() {
   );
 }
 
-app.on('ready', createWindow);
+
+app.whenReady().then(() => {
+	createWindow();
+	
+  	// 업데이트 사항을 체크하고 알려주는 메서드
+	autoUpdater.checkForUpdatesAndNotify();
+});
+
+ipcMain.on("app_version", (event) => {
+  	// app_version 채널로 현재 version을 보내는 코드
+	event.sender.send("app_version", { version: app.getVersion() });
+});
+
+autoUpdater.on("update-available", () => {
+	win.webContents.send("update_available");
+});
+
+autoUpdater.on("update-downloaded", () => {
+	win.webContents.send("update_downloaded");
+});
+
+ipcMain.on("restart_app", () => {
+  	// restart_app 채널로 수신 받았을 때
+  	// autoUpdater의 함수를 사용하여
+  	// 일렉트론 앱 종료 후 최신 버전으로 다시 설치
+	autoUpdater.quitAndInstall();
+});
+
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit(); // macOS에서 앱 종료 처리
--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link, useNavigate } from "react-router-dom";
 
 function Home() {
@@ -21,6 +22,61 @@ function Contact() {
 }
 
 function App() {
+
+  useEffect(() => {
+    // 여기에 실행할 코드 작성
+    console.log('Component has rendered or updated');
+
+    const ipcRenderer = window.require('electron').ipcRenderer;
+    const version = document.querySelector("#version");
+
+    ipcRenderer.send("app_version"); // app_version 채널로 송신
+    // app_version 채널에서 송신
+    ipcRenderer.on("app_version", (event, data) => { 
+        // event 모두 종료 후 다음 코드 실행
+      ipcRenderer.removeAllListeners("app_version");
+        // 업데이트가 필요한 경우 현재 버전을 알려주는 메시지입니다.
+      version.innerText = `현재 버전:  ${data.version}`;
+    });
+
+    const notification = document.querySelector(".notification");
+    const message = document.querySelector(".update-message");
+    const closeButton = document.querySelector("#close-button");
+    const restartButton = document.querySelector("#restart-button");
+
+    ipcRenderer.on("update_available", () => {
+      ipcRenderer.removeAllListeners("update_available");
+      message.innerText = "업데이트 파일을 다운로드 중입니다...";
+      notification.classList.remove("hidden");
+    });
+
+    // update_downloaded 채널로 송/수신
+    ipcRenderer.on("update_downloaded", () => {
+      ipcRenderer.removeAllListeners("update_downloaded");
+        // 업데이트 완료 후 해당 메시지를 보여줍니다.
+      message.innerText =
+        "업데이트 파일 다운로드를 마쳤습니다. \n 재시작을 하면 업데이트 버전이 실행됩니다. \n 재시작 하시겠습니까?";
+        // 닫기 / 재시작 버튼을 렌더링합니다.
+      restartButton.classList.remove("hidden");
+      notification.classList.remove("hidden");
+    });
+
+    function closeNotification() {
+      notification.classList.add("hidden");
+    }
+    function restartApp() {
+      ipcRenderer.send("restart_app");
+    }
+
+    closeButton.addEventListener("click", closeNotification);
+    restartButton.addEventListener("click", restartApp);
+
+    // 선택적으로 클린업(cleanup) 함수를 반환
+    return () => {
+      console.log('Component will unmount or re-run useEffect');
+    };
+  });
+
   return (
     <Router>
       <nav>
@@ -35,6 +91,28 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
+      <div>
+        version: 1.1
+      </div>
+
+      <div class="notification hidden">
+        <div class="notification-modal">
+          <h2 class="modal-title"> 업데이트 안내 </h2>
+          <h2 id="version"> </h2>
+          <p class="update-message"></p>
+          <div class="updater-btn-wrapper">
+            <button id="close-button" class="btn btn-secondary hidden"> 닫기 </button>
+            <button id="restart-button" class="btn btn-primary hidden"> 재시작 </button>
+          </div>
+        </div>
+      </div>
+      <div class="loading-wrapper hidden">
+        <div class="loading-container">
+          <div class="loading"></div>
+          <div id="loading-text">loading</div>
+        </div>
+      </div>
+      
     </Router>
   );
 }
